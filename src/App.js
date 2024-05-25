67d83d6f8be77a08e8bb1a14a79f1cf49af75d29
74NV1R <mhasan182021@bscse.uiu.ac.bd>
2024-05-25 19:13:10 +0600

    folders created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
+import { useState } from 'react'
 import './App.css'
-import Header from './components/header/Header'
 import { BrowserRouter as Router, RouterProvider, Routes, Route, Link } from 'react-router-dom'
-import Home from './components/pages/login/Home'
 
 function App() {
+
   return (
     <div className="App">
-      <Router>
-        <Header/>
-        <Routes>
-          <Route path = '/' element={<Home/>}/>
-        </Routes>
-      </Router>
+      Hello 
     </div>
   )
 }
